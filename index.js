ec38077f732783177f2a174207b4f22792e9c73e
Peter Muller <peterianmuller@gmail.com>
2017-04-21 15:14:51 -0700

    Prepared for heroku deployment

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/', express.static(__dirname));
+
+
+app.get('/', function (req, res) {
+  res.sendFile('/index.html');
+});
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('peterianmuller.com is live!');
+});
