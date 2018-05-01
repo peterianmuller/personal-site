a794ad9be7db96640c72c5b7e8caf10bd3f6ec7d
peterianmuller <peterianmuller@gmail.com>
2018-04-30 17:20:00 -0700

    Add ajax call to reddit api to have user preference for images

--- reddit-api/main.js
+++ reddit-api/main.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 	$('.btn').click(function() {
-		console.log('hi');
+		let userInput = $('.user-input').val();
+		$('.user-input').val('loading. . .');
+		$.ajax({
+			method: 'GET',
+			data: {
+				q: userInput,
+				restrict_sr: 'true'
+			},
+			url: 'https://www.reddit.com/r/aww/search.json',
+			success: function(response) {
+				let pics = response.data.children;
+				$('.user-input').val('');
+				pics.forEach(function(pic) {
+					console.log(pic.data);
+					var img = $('<img id="dynamic">').attr('src', pic.data.thumbnail);
+					$('.list').append(img);
+				});
+			}
+		});
 	});
 });
