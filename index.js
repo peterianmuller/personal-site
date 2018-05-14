cb9160244cfad5532e731d248e915f0f42dce16a
peterianmuller <peterianmuller@gmail.com>
2018-05-14 16:07:16 -0700

    Actually removed route and directory with noah image

--- index.js
+++ index.js
@@ -13,31 +13,6 @@ app.get('/', function(req, res) {
 	res.sendFile('/index.html');
 });
 
-app.get('/music', function(req, res) {
-	res.sendFile('/music/index.html');
-});
-
-app.get('/blog', function(req, res) {
-	res.sendFile(`${__dirname}/blog/blogroll.html`);
-});
-
-app.get('/spanish', function(req, res) {
-	res.sendFile(`spanish/index.html`);
-});
-
-app.get('/reddit', function(req, res) {
-	console.log('hello');
-	res.sendFile(`${__dirname}/reddit-api/index.html`);
-});
-
-app.get('/jm-photo', function(req, res) {
-	res.sendFile('jm-photo/index.html');
-});
-
-app.get('/big-noah', function(req, res) {
-	res.sendFile('big-bass/index.html');
-});
-
 // port assignment
 
 app.listen(process.env.PORT || 3000, function() {
