221bbef4f43c74e1328172ad24e25c6b942c1541
peterianmuller <peterianmuller@gmail.com>
2018-05-15 11:04:49 -0700

    Remove get request to fall back on express.static

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/', express.static(__dirname));
 
-app.get('/', function(req, res) {
-	res.sendFile('index.html');
-});
+// app.get('/', function(req, res) {
+// 	res.sendFile('index.html');
+// });
 
 app.listen(process.env.PORT || 3000, function() {
 	console.log('peterianmuller.com is live!');
