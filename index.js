c4452515453e2c5cd8c0d71165ec2abaf5e6bf61
peterianmuller <peterianmuller@gmail.com>
2018-07-26 13:26:07 -0700

    Added text file for SSL

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/', express.static(__dirname));
 
-// app.get('/', function(req, res) {
-// 	res.sendFile('index.html');
-// });
+app.get('/.well-known/acme-challenge/Na6vwinTMeIh-E20kQR0A2jinu2Qxbf3xeDkHhHFPtM', function(req, res) {
+	res.sendFile(`${__dirname}/cert-file.txt`);
+});
 
 app.listen(process.env.PORT || 3000, function() {
 	console.log('peterianmuller.com is live!');
