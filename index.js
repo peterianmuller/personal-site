53eeaec0af8cb2cf079f41fe2136c5c28fad666e
peterianmuller <peterianmuller@gmail.com>
2018-07-30 10:25:58 -0700

    Update index.js with routes for certificate

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/', express.static(__dirname));
 
+app.get(`/.well-known/acme-challenge/QVKUIxs2RvIpleykacTJqX4-kXgK81jCQLbKdr1E-8s`, (req, res) => {
+	res.send(`${__dirname}/cert.txt`);
+});
+
 app.listen(process.env.PORT || 3000, function() {
 	console.log('peterianmuller.com is live!');
 });
