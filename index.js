7216a7b4475812b50b336b898eb002c4c57d245d
peterianmuller <peterianmuller@gmail.com>
2018-07-31 10:01:39 -0700

    Updating route for ssl

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/', express.static(__dirname));
 
-app.get(`/.well-known/acme-challenge/Zzam5hmy_4ckg6bzMfUCtPmEeg8s59k2GTWCdsfqXuE`, (req, res) => {
-	res.send(`Zzam5hmy_4ckg6bzMfUCtPmEeg8s59k2GTWCdsfqXuE.5iP72yFceQ2O41wwpICeewxefMnleNW-FVoYnKGHl0Y`);
-});
+// app.get(`/.well-known/acme-challenge/Zzam5hmy_4ckg6bzMfUCtPmEeg8s59k2GTWCdsfqXuE`, (req, res) => {
+// 	res.send(`Zzam5hmy_4ckg6bzMfUCtPmEeg8s59k2GTWCdsfqXuE.5iP72yFceQ2O41wwpICeewxefMnleNW-FVoYnKGHl0Y`);
+// });
 
-app.get(`/.well-known/acme-challenge/pLEqmhJ6Xy7ahp4uyqcV2phu7sTZGkictC9UbH6KnT0`, (req, res) => {
-	res.send(`pLEqmhJ6Xy7ahp4uyqcV2phu7sTZGkictC9UbH6KnT0.5iP72yFceQ2O41wwpICeewxefMnleNW-FVoYnKGHl0Y`);
-});
+// app.get(`/.well-known/acme-challenge/pLEqmhJ6Xy7ahp4uyqcV2phu7sTZGkictC9UbH6KnT0`, (req, res) => {
+// 	res.send(`pLEqmhJ6Xy7ahp4uyqcV2phu7sTZGkictC9UbH6KnT0.5iP72yFceQ2O41wwpICeewxefMnleNW-FVoYnKGHl0Y`);
+// });
 
 // both root domain and www
 app.get(`/.well-known/acme-challenge/Zzam5hmy_4ckg6bzMfUCtPmEeg8s59k2GTWCdsfqXuE`, (req, res) => {
