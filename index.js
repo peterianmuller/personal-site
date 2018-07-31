e759671b32e8ed2a7cd930f33be068e6485f8fc2
peterianmuller <peterianmuller@gmail.com>
2018-07-31 09:58:19 -0700

    Add 2nd route for ssl for root and www

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ app.get(`/.well-known/acme-challenge/pLEqmhJ6Xy7ahp4uyqcV2phu7sTZGkictC9UbH6KnT0
 app.get(`/.well-known/acme-challenge/Zzam5hmy_4ckg6bzMfUCtPmEeg8s59k2GTWCdsfqXuE`, (req, res) => {
 	res.send(`Zzam5hmy_4ckg6bzMfUCtPmEeg8s59k2GTWCdsfqXuE.5iP72yFceQ2O41wwpICeewxefMnleNW-FVoYnKGHl0Y`);
 });
+// both root domain and www
+app.get(`/.well-known/.well-known/acme-challenge/mk9rsBekWIQik5sbaXLumA53Q7txiulTG7xnWWRuxiM`, (req, res) => {
+	res.send(`mk9rsBekWIQik5sbaXLumA53Q7txiulTG7xnWWRuxiM.5iP72yFceQ2O41wwpICeewxefMnleNW-FVoYnKGHl0Y`);
+});
 
 app.listen(process.env.PORT || 3000, function() {
 	console.log('peterianmuller.com is live!');
