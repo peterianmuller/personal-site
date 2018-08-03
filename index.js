1644290b357260801716db71212cad7e8986e854
peterianmuller <peterianmuller@gmail.com>
2018-08-03 12:42:55 -0700

    Debudding nba api call on heroku

--- index.js
+++ index.js
@@ -54,13 +54,14 @@ app.get('/player-comparison', (req, res) => {
 	console.log(NBA.findPlayer);
 	console.log(player1);
 	console.log(player2);
+	console.log(N`BA.stats.playerInfo is:${NBA.stats.playerInfo}`);
 	NBA.stats.playerInfo({ PlayerID: player1.playerId }).then(response => {
-		console.log(response);
+		console.log(`1st response is: ${response}`);
 		playersInfoAndStat.push(response);
 		NBA.stats.playerInfo({ PlayerID: player2.playerId }).then(response => {
-			console.log(response);
+			console.log(`2nd response is: ${response}`);
 			playersInfoAndStat.push(response);
-			console.log(playersInfoAndStat);
+			console.log(`playersInfoAndStat:${playersInfoAndStat}`);
 			res.send(playersInfoAndStat);
 		});
 	});
